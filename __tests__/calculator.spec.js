99a83471fc2d30c6ba2915d5e6ccc93c80595cfa
oscarporras02 <oscarrussi@outlook.com>
2020-12-22 22:35:34 -0500

    add tests for sum, substract, product and pslit methods

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -19,6 +19,7 @@ describe("testing capitalize method", () => {
         expect(calculator.capitalize('')).toEqual('');
     });
   });
+
   describe("testing reverseString method", () => {
     test("converts hello to olleh", () => {
         expect(calculator.reverseString('hello')).toEqual('olleh');
@@ -31,5 +32,119 @@ describe("testing capitalize method", () => {
     });
     test("if receive a void string then don't modify it", () => {
         expect(calculator.reverseString('')).toEqual('');
+    });
+  });
+
+  describe("testing sum method", () => {
+    test("2+3=5", () => {
+        expect(calculator.sum(2,3)).toEqual(5);
+    });
+    test("2+-3=-1", () => {
+        expect(calculator.sum(2,-3)).toEqual(-1);
+    });
+    test("null if a==null", () => {
+        expect(calculator.sum(null, 3)).toEqual(null);
+    });
+    test("null if b==null", () => {
+        expect(calculator.sum(3,null)).toEqual(null);
+    });
+    test("null if a and b equals null", () => {
+        expect(calculator.sum(null,null)).toEqual(null);
+    });
+    test("null if a=='hello'", () => {
+        expect(calculator.sum('hello',3)).toEqual(null);
+    });
+    test("null if b=='hello'", () => {
+        expect(calculator.sum(2,'hello')).toEqual(null);
+    });
+    test("null if a and b equals 'hello'", () => {
+        expect(calculator.sum('hello','hello')).toEqual(null);
+    });
+  });
+
+  describe("testing sum method", () => {
+    test("2-3=-1", () => {
+        expect(calculator.subtract(2,3)).toEqual(-1);
+    });
+    test("2--3=-1", () => {
+        expect(calculator.subtract(2,-3)).toEqual(5);
+    });
+    test("null if a==null", () => {
+        expect(calculator.subtract(null, 3)).toEqual(null);
+    });
+    test("null if b==null", () => {
+        expect(calculator.subtract(3,null)).toEqual(null);
+    });
+    test("null if a and b equals null", () => {
+        expect(calculator.subtract(null,null)).toEqual(null);
+    });
+    test("null if a=='hello'", () => {
+        expect(calculator.subtract('hello',3)).toEqual(null);
+    });
+    test("null if b=='hello'", () => {
+        expect(calculator.subtract(2,'hello')).toEqual(null);
+    });
+    test("null if a and b equals 'hello'", () => {
+        expect(calculator.subtract('hello','hello')).toEqual(null);
+    });
+  });
+
+  describe("testing product method", () => {
+    test("2*3=6", () => {
+        expect(calculator.product(2,3)).toEqual(6);
+    });
+    test("2*-3=-6", () => {
+        expect(calculator.product(2,-3)).toEqual(-6);
+    });
+    test("null if a==null", () => {
+        expect(calculator.product(null, 3)).toEqual(null);
+    });
+    test("null if b==null", () => {
+        expect(calculator.product(3,null)).toEqual(null);
+    });
+    test("null if a and b equals null", () => {
+        expect(calculator.product(null,null)).toEqual(null);
+    });
+    test("null if a=='hello'", () => {
+        expect(calculator.product('hello',3)).toEqual(null);
+    });
+    test("null if b=='hello'", () => {
+        expect(calculator.product(2,'hello')).toEqual(null);
+    });
+    test("null if a and b equals 'hello'", () => {
+        expect(calculator.product('hello','hello')).toEqual(null);
+    });
+  });
+
+  describe("testing split method", () => {
+    test("2/3=0.66666", () => {
+        expect(calculator.split(2,3)).toEqual(2/3);
+    });
+    test("2*-3=-6", () => {
+        expect(calculator.split(2,-3)).toEqual(-2/3);
+    });
+    test("0/0 = undefined", () => {
+        expect(calculator.split(0,0)).toEqual(undefined);
+    });
+    test("4/0 = undefined", () => {
+        expect(calculator.split(4,0)).toEqual(undefined);
+    });
+    test("null if a==null", () => {
+        expect(calculator.split(null, 3)).toEqual(null);
+    });
+    test("null if b==null", () => {
+        expect(calculator.split(3,null)).toEqual(null);
+    });
+    test("null if a and b equals null", () => {
+        expect(calculator.split(null,null)).toEqual(null);
+    });
+    test("null if a=='hello'", () => {
+        expect(calculator.split('hello',3)).toEqual(null);
+    });
+    test("null if b=='hello'", () => {
+        expect(calculator.split(2,'hello')).toEqual(null);
+    });
+    test("null if a and b equals 'hello'", () => {
+        expect(calculator.split('hello','hello')).toEqual(null);
     });
   });
--- src/calculator.js
+++ src/calculator.js
@@ -6,6 +6,23 @@ const calculator = {
     reverseString : (text)=>{
         if(	typeof(text) == 'string') return text.split('').reverse().join('');
         else return text;
+    },
+    sum : (a, b)=>{
+        if(	typeof(a) == 'number' && typeof(b) == 'number') return a+b;
+        else return null;
+    },
+    subtract : (a,b)=>{
+        if(	typeof(a) == 'number' && typeof(b) == 'number') return a-b;
+        else return null;
+    },
+    product : (a,b)=>{
+        if(	typeof(a) == 'number' && typeof(b) == 'number') return a*b;
+        else return null;
+    },
+    split : (a,b)=>{
+        if(	typeof(a) != 'number' || typeof(b) != 'number') return null;
+        else if (b==0) return undefined;
+        else return a/b;
     }
 }
   export default calculator;
